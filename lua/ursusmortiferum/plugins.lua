d57df1cc67414f69bb77e160f035a33d4a494b28
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2023-08-12 16:53:03 +0200

    Plugin clean-up part xXx

--- lua/ursusmortiferum/plugins.lua
+++ lua/ursusmortiferum/plugins.lua
@@ -120,6 +120,17 @@ return {
     opts = {}
   },
 
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+      { 'folke/neodev.nvim',       opts = {} },
+    },
+  },
+
   -- Fuzzy Finder (files, lsp, etc)
   {
     'nvim-telescope/telescope.nvim',
