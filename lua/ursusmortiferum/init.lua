c063cc4544fc87cefaa96e77cbded043e916d11f
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2023-08-17 23:03:21 +0200

    Mostly lazy update

--- lua/ursusmortiferum/init.lua
+++ lua/ursusmortiferum/init.lua
@@ -1,8 +1,8 @@
-require('ursusmortiferum.settings')
-require('ursusmortiferum.remap')
-require('ursusmortiferum.plugins')
-require('ursusmortiferum.functions')
-require('ursusmortiferum.autocmds')
+require 'ursusmortiferum.settings'
+require 'ursusmortiferum.remap'
+require 'ursusmortiferum.plugins'
+require 'ursusmortiferum.functions'
+require 'ursusmortiferum.autocmds'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
@@ -20,6 +20,40 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   { import = 'ursusmortiferum.plugins' },
 }, {})
+
+require('lazy').setup {
+  spec = {
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = true },
+  dev = {
+    -- directory where you store your local plugin projects
+    path = "~/projects",
+    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+    patterns = {},    -- For example {"folke"}
+    fallback = false, -- Fallback to git when local plugin doesn't exist
+  },
+  install = {
+    missing = true,
+    -- try to load one of these colorschemes when starting an installation during startup
+    colorscheme = { 'tokyonight-night' },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/ursusmortiferum/remap.lua
+++ lua/ursusmortiferum/remap.lua
@@ -1,56 +1,57 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Explore' })
+local keymap = vim.keymap.set
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+keymap('n', '<leader>pv', vim.cmd.Ex, { desc = 'Explore' })
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+keymap('v', 'J', ":m '>+1<CR>gv=gv")
+keymap('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+keymap('n', '<C-d>', '<C-d>zz')
+keymap('n', '<C-u>', '<C-u>zz')
 
-vim.keymap.set('x', '<leader>p', [["_dP]])
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'Nzzzv')
+
+keymap('x', '<leader>p', [["_dP]])
 
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
+keymap('n', '<leader>Y', [["+Y]])
 
-vim.keymap.set('n', '<leader><leader>', function()
+keymap('n', '<leader><leader>', function()
     vim.cmd('so')
 end)
 
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' })
+keymap('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat' })
 
-vim.keymap.set('i', '<C-c>', '<Esc>')
+keymap('i', '<C-c>', '<Esc>')
 
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
 
 -- Yes, make it rain -> it may help you cope with the reality of the world
-vim.keymap.set('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>', { desc = 'Make it rain' })
+keymap('n', '<leader>mr', '<cmd>CellularAutomaton make_it_rain<CR>', { desc = 'Make it rain' })
 
-vim.keymap.set('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
-vim.keymap.set('n', '<leader>tr', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh NvimTree' })
-
+keymap('n', '<leader>tt', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+keymap('n', '<leader>tr', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh NvimTree' })
 --
 --
 --
-vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+keymap('n', 'Q', '<nop>')
+keymap('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>') --, { silent = true })
-vim.keymap.set('n', '<leader>x', '<cmd>!python %<CR>')
+keymap('n', '<leader>x', '<cmd>!chmod +x %<CR>') --, { silent = true })
+keymap('n', '<leader>x', '<cmd>!python %<CR>')
 -- Join the current line with the line below it
--- vim.keymap.set('n', 'J', "mzJ'z")
+-- keymap('n', 'J', "mzJ'z")
 -- Opens file in a specified path
--- vim.keymap.set('n', '<leader>vpp', "<cmd>e ~/path<CR>");
-vim.keymap.set('n', '<leader>vwm', function()
+-- keymap('n', '<leader>vpp', "<cmd>e ~/path<CR>");
+keymap('n', '<leader>vwm', function()
     require('vim-with-me').StartVimWithMe()
 end)
-vim.keymap.set('n', '<leader>svwm', function()
+keymap('n', '<leader>svwm', function()
     require('vim-with-me').StopVimWithMe()
 end)
 
@@ -61,11 +62,11 @@ end)
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
