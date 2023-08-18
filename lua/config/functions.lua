3475157f932d437d5a57346e0adb9daea1400782
Lewinski <101043729+UrsusMortiferum@users.noreply.github.com>
2023-08-18 11:07:41 +0200

    + SQL-related function

--- lua/config/functions.lua
+++ lua/config/functions.lua
@@ -1,34 +1,14 @@
--- local M = {}
---
--- function ListToSQLFragment(elements)
---   local fragment = "(" .. table.concat(elements, "', '") .. "')"
---   return fragment
--- end
---
--- return M
---
-local M = {}
+function ListToSQLString()
+    local s_line, e_line = vim.fn.line("'<"), vim.fn.line("'>")
+    local lines = vim.api.nvim_buf_get_lines(0, s_line - 1, e_line, false)
 
-function M.ListToQuotedParantheses()
-  local selected = vim.fn.getline("'<", "'>")
-  local quoted = {}
+    local flines = {}
+    for _, line in ipairs(lines) do
+        table.insert(flines, "'" .. line .. "'")
+    end
 
-  for _, line in ipairs(selected) do
-    table.insert(quoted, "('" .. line .. "')")
-  end
-
-  local first = vim.fn.line("'<")
-
-  for i, quoted in ipairs(quoted) do
-    vim.fn.setline(first + i - 1, quoted)
-  end
-
-  vim.fn.setpos('.', { 0, first + #quoted - 1, 0, 0 })
+    local fstring = "(" .. table.concat(flines, ", ") .. ")"
+    vim.api.nvim_buf_set_lines(0, s_line - 1, e_line, false, { fstring })
 end
 
-return M
-
--- function ListToSQLFragment(elements)
---  local fragment = "(" .. table.concat(elements, "', '") .. "')"
---  return fragment
--- end
+vim.api.nvim_set_keymap('v', '<leader>b', ':lua ListToSQLString()<CR>', { noremap = true, silent = true })
