fc9c4a4f7781481e51fec064d9001d20f7e4358c
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2023-08-24 22:06:21 +0200

    Python config for macos - done; Codestats - reverted to github plugin

--- lua/plugins/codestats.lua
+++ lua/plugins/codestats.lua
@@ -1,23 +1,18 @@
 return {
+    --     {
+    --         'https://gitlab.com/code-stats/code-stats-vim',
+    --         config = function()
+    --             vim.g.codestats_api_key =
+    --             'SFMyNTY.VlhKemRYTmZUVzl5ZEdsbVpYSjFiUT09IyNNakEwT0RRPQ.4Eg9Lr8VH94F9SP1Ul7IYGnMEr-fdKfxw1kZjvEmey8'
+    --         end,
+    --     },
     {
         'YannickFricke/codestats.nvim',
-        -- config = function()
-        --     require('codestats-nvim').setup({
         depedencies = {
             'nvim-lua/plenary.nvim',
         },
         opts = {
             token = 'SFMyNTY.VlhKemRYTmZUVzl5ZEdsbVpYSjFiUT09IyNNakEwT0RRPQ.4Eg9Lr8VH94F9SP1Ul7IYGnMEr-fdKfxw1kZjvEmey8',
         },
-        -- })
-        -- end,
-        -- requires = { 'nvim-lua/plenary.nvim' }
     },
-    -- {
-    --     'https://gitlab.com/code-stats/code-stats-vim',
-    --     opts = {
-    --         codestats_api_key =
-    --         'SFMyNTY.VlhKemRYTmZUVzl5ZEdsbVpYSjFiUT09IyNNakEwT0RRPQ.4Eg9Lr8VH94F9SP1Ul7IYGnMEr-fdKfxw1kZjvEmey8',
-    --     },
-    -- },
 }
