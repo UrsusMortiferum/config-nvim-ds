ad3376e3e8f3f6f22b7cd8b5b1f3ddcf77993cd3
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2023-08-28 22:36:57 +0200

    some changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,25 @@ return {
     -- Fuzzy Finder (files, lsp, etc)
     {
         "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
+        -- branch = "0.1.x",
         dependencies = { "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons" }
+            "nvim-tree/nvim-web-devicons" },
+        cmd = "Telescope",
+        -- keys = {
+        --     { "<leader>?",       require("telescope.builtin").oldfiles,    { desc = "[?] Find recently opened files" } },
+        --     { "<leader><space>", require("telescope.builtin").buffers,     { desc = "[ ] Find existing buffers" } },
+        --     { "<leader>gf",      require("telescope.builtin").git_files,   { desc = "Search [G]it [F]iles" } },
+        --     { "<leader>sf",      require("telescope.builtin").find_files,  { desc = "[S]earch [F]iles" } },
+        --     { "<leader>sh",      require("telescope.builtin").help_tags,   { desc = "[S]earch [H]elp" } },
+        --     { "<leader>sw",      require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" } },
+        --     { "<leader>sg",      require("telescope.builtin").live_grep,   { desc = "[S]earch by [G]rep" } },
+        --     { "<leader>sd",      require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" } },
+            --     { "<leader>/", function()  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+            --             winblend = 10,
+            --             previewer = false,
+            --         })
+            --     end, { desc = "[/] Fuzzily search in current buffer" } },
+        -- },
     },
 
     -- Fuzzy Finder Algorithm which requires local dependencies to be built.
